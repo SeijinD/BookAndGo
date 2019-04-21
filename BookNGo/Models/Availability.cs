35882ec76f5b079f3589cdfbf5c72d607b3816c2
SeijinD <seijindemon@protonmail.com>
2019-04-21 16:42:51 +0300

    add models

---
+++ BookNGo/Models/Availability.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookNGo.Models
+{
+    public class Availability
+    {
+        [Key]
+        public int AvailabilityId { get; set; }
+
+        public House HouseId { get; set; } //ενα σπιτι εχει πολλες διαθεσιμες μερες(1-n)
+
+        [Display(Name = "Start Date")]
+        [Required(ErrorMessage = "Start Date Required")]
+        public DateTime StartDate { get; set; }
+
+        [Display(Name = "First Name")]
+        [Required(ErrorMessage = "First Name Required")]
+        public DateTime EndDate { get; set; }
+
+        public virtual ICollection<House> Houses{ get; set; }//πολλα χαρακτηριστικα ανηκουν σε πολλα σπιτια(n-n)
+    }
+}
