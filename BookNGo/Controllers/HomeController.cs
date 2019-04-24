7d7af7f60d9400781cda472484d6d07a41b4def1
SeijinD <seijindemon@protonmail.com>
2019-04-24 18:37:38 +0300

    change Migration add image folder with 4 images

--- BookNGo/Controllers/HomeController.cs
+++ BookNGo/Controllers/HomeController.cs
@@ -19,12 +19,6 @@ namespace BookNGo.Controllers
 
             return View();
         }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+        
     }
 }
